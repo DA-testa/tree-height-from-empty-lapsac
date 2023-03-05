28cb02564205949c1927fd2d0b5a3385c16f9af3
Lauma Gailīte <123964250+lapsac@users.noreply.github.com>
2023-03-06 01:03:43 +0200

    Update tree_height.py
    should work

--- tree_height.py
+++ tree_height.py
@@ -1,30 +1,49 @@
-# python3
-
 import sys
 import threading
-
+import numpy as np
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    tree = np.empty((n,), dtype = object)
+    root == -1
 
+    for i, parent in enumerate(parents):
+        if parent == -1:
+            root = i
+        else:
+            if tree[parent] is None:
+                tree[parent] = [i]
+            else:
+                tree[parent].append(i)
+
+    def augst(node):
+        if tree[node] is None:
+            return 1
+        return max([height(child) for child in koks[node]], default = 0 ) +1
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    input_type = input("input 'i' or 'f': ")
+
+    if input_type == 'i':
+        n = int(input("Enter the number of elements: "))
+        parents = list(map(int, input("enter parents of nodes")))
+
+    elif input_type == 'f':
+        filename = input("Enter file name: ").strip()
+        if 'a' in filename:
+            print("File name can't contain letter 'a'. ")
+            return
+        
+        with open(f"./test/{filename}", 'r') as f:
+            n = int(f.readline().strip())
+            parents = list(map(int, f.readline().split()))
+
+    else:
+        print("invalid input. ")
+        return
+
+    print(compute_height(n, parents))
 
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
